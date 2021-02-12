c069943cf7ec073ff614b7fe61f9fecc508dc3ed
Alexandr <valverin95@gmail.com>
2021-02-12 18:17:20 +0300

    refactor: [pages.main] import all-countries table

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,27 +1,14 @@
-import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { ApiService } from '@app/services/api.service';
+import { Component, OnInit } from '@angular/core';
 import { select, Store } from '@ngrx/store';
-import { selectCases, selectRegionsCases } from '@app/store/cases/cases.selectors';
-import { map, takeUntil } from 'rxjs/operators';
-import { MatSort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
-import { ICountryStateAll } from '@app/models/cases/country';
-import { Subject } from 'rxjs';
-import { MatPaginator } from '@angular/material/paginator';
+import { selectCases } from '@app/store/cases/cases.selectors';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent implements OnInit, AfterViewInit, OnDestroy {
-
-  @ViewChild(MatSort) sort: MatSort;
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-
-  public displayedColumns: string[] = ['country', 'population', 'confirmed', 'deaths', 'actions'];
-  public dataSource: MatTableDataSource<ICountryStateAll>;
-  public countries: ICountryStateAll[];
+export class MainComponent implements OnInit {
 
   public cases$ = this.store.pipe(select(selectCases)).pipe(
     map(list => Object.values(list)
@@ -30,40 +17,11 @@ export class MainComponent implements OnInit, AfterViewInit, OnDestroy {
     )
   );
 
-  public regions$ = this.store.pipe(select(selectRegionsCases));
-
-  private destroy$ = new Subject();
-
   constructor(
-    private api: ApiService,
     private store: Store
   ) {
   }
 
   ngOnInit(): void {
-    this.cases$
-      .pipe(takeUntil(this.destroy$))
-      .subscribe(countries => {
-        this.dataSource = new MatTableDataSource<ICountryStateAll>(countries);
-      });
-  }
-
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
-  }
-
-  applyFilter($event: KeyboardEvent) {
-    const filterValue = ($event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-    }
-  }
-
-  ngOnDestroy() {
-    this.destroy$.next();
-    this.destroy$.complete();
   }
 }
--- src/app/pages/main/main.module.ts
+++ src/app/pages/main/main.module.ts
@@ -2,32 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
-import { MatTableModule } from '@angular/material/table';
 
 import { MainComponent } from './main.component';
-import { MatSortModule } from '@angular/material/sort';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatButtonModule } from '@angular/material/button';
-import { CountryFlagModule } from '@app/shared/components/country-flag/country-flag.module';
-import { MatPaginatorModule } from '@angular/material/paginator';
-
+import { AllCountriesStatTableModule } from '@app/shared/components/all-countries-stat-table/all-countries-stat-table.module';
 
 
 @NgModule({
-  declarations: [MainComponent],
+  declarations: [
+    MainComponent
+  ],
   imports: [
     CommonModule,
     MainRoutingModule,
-    MatTableModule,
-    MatSortModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSelectModule,
-    MatButtonModule,
-    CountryFlagModule,
-    MatPaginatorModule
+    AllCountriesStatTableModule
   ]
 })
 export class MainModule {
