bc37c6787362eb3661ea26bcc13586b528c0d02f
Alexandr <valverin95@gmail.com>
2021-02-16 21:53:31 +0300

    feat: [page.regions] added line charts to regions

--- src/app/pages/page-regions/page-regions.component.ts
+++ src/app/pages/page-regions/page-regions.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 import { selectRegionsCases } from '@app/store/cases/cases.selectors';
+import { ChartComponent } from 'ng-apexcharts';
+
 
 @Component({
   selector: 'app-page-regions',
@@ -9,13 +11,44 @@ import { selectRegionsCases } from '@app/store/cases/cases.selectors';
 })
 export class PageRegionsComponent implements OnInit {
 
-  public regions$ = this.store.pipe(select(selectRegionsCases))
+  public regions$ = this.store.pipe(select(selectRegionsCases));
+
+  @ViewChild('chart') chart: ChartComponent;
+  public chartOptions: Partial<ChartComponent>;
 
   constructor(
     private store: Store
-  ) { }
+  ) {
+  }
 
   ngOnInit(): void {
+    this.regions$.subscribe(cases => {
+      const data: number[] = [];
+      const categories: string[] = [];
+
+      cases.forEach((region, name) => {
+        data.push(region.recovered);
+        categories.push(name);
+      });
+
+      this.createGraph(data, categories);
+    });
+  }
+
+  createGraph(data: number[] = [], categories: string[] = []) {
+    this.chartOptions = {
+      series: [{
+        name: 'Recovered',
+        data
+      }],
+      chart: {
+        height: 350,
+        type: 'bar'
+      },
+      xaxis: {
+        categories
+      }
+    };
   }
 
 }
--- src/app/pages/page-regions/page-regions.module.ts
+++ src/app/pages/page-regions/page-regions.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { PageRegionsRoutingModule } from './page-regions-routing.module';
 import { PageRegionsComponent } from './page-regions.component';
 import { RegionsLinksGridComponent } from './regions-links-grid/regions-links-grid.component';
+import { NgApexchartsModule } from 'ng-apexcharts';
 
 
 @NgModule({
   declarations: [PageRegionsComponent, RegionsLinksGridComponent],
   imports: [
     CommonModule,
-    PageRegionsRoutingModule
+    PageRegionsRoutingModule,
+    NgApexchartsModule
   ]
 })
 export class PageRegionsModule { }
