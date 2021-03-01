e5ecaf67f6351c358c9bfd986d6a57779e1330ff
Alexandr <valverin95@gmail.com>
2021-03-01 20:04:36 +0300

    feat: [header.search] parse cases for search field

--- src/app/core/template/header/header.component.ts
+++ src/app/core/template/header/header.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { map, startWith } from 'rxjs/operators';
+import { map, startWith, switchMap } from 'rxjs/operators';
+import { select, Store } from '@ngrx/store';
+import { selectCases } from '@app/store/cases/cases.selectors';
+import { getStoreValue } from '@app/store/store.helpers';
+import { ICases } from '@app/models/cases/country';
 
 @Component({
   selector: 'app-header',
@@ -13,20 +17,22 @@ export class HeaderComponent implements OnInit {
   @Output() onToggleSidebar = new EventEmitter();
 
   control = new FormControl();
-  options: string[] = ['One', 'Two', 'Three'];
+  options: ICases;
   filteredOptions: Observable<string[]>;
 
-  constructor() { }
+  cases$ = this.store.pipe(select(selectCases));
+
+  constructor(
+    private store: Store
+  ) {
+  }
 
   ngOnInit(): void {
     this.filteredOptions = this.control.valueChanges.pipe(
       startWith(''),
-      map(value => this.filter(value))
+      switchMap((value) => this.cases$.pipe(
+        map(cases => Object.keys(cases).filter(item => item.includes(value))))
+      )
     );
-  }
-
-  private filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
-    return this.options.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
   }
 }
